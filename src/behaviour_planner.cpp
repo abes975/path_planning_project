fea0353a7e830b975c0fa3601e40e3d5f79bbe72
seba <seba@hikari.be-secure.it>
2017-08-11 16:30:20 +0200

    Start adding planner

--- src/behaviour_planner.cpp
+++ src/behaviour_planner.cpp
@@ -6,4 +6,30 @@
 BehaviourPlanner::BehaviourPlanner(Style::DriveStyle style)
 {
   _style = style;
+  _num_lanes = lanes;
+}
+
+bool BehaviourPlanner::is_too_close()
+{
+
+}
+
+bool BehaviourPlanner::is_adjacent_lane_safe(int lane)
+{
+
+}
+
+void BehaviourPlanner next_action(double ego_pos, vector<vector<double> sensor_fusion)
+{
+
+}
+
+int BehaviourPlanner::get_lane() const
+{
+    return _lane;
+}
+
+int BehaviourPlanner::get_speed() const
+{
+    return _speed;
 }
--- src/behaviour_planner.hpp
+++ src/behaviour_planner.hpp
@@ -6,14 +6,23 @@ namespace Style
     enum DriveStyle { CALM, SPORTY, GRANNY };
 };
 
+
 using namespace std;
 
 class BehaviourPlanner
 {
   private:
     Style::DriveStyle _style;
+    double _speed;
+    int _lane;
+    bool is_too_close();
+    bool is_adjacent_lane_safe(int lane);
+
   public:
     BehaviourPlanner(Style::DriveStyle style);
+    int get_lane() const;
+    double get_speed() const;
+    void next_action(double ego_pos, vector<vector<double> sensor_fusion);
 };
 
 #endif
